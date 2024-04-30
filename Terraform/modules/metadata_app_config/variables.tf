f203f19070b30f2c1e4fb685bf092ae246a8f771
e.rose <reconroseeri@gmail.com>
2024-04-30 18:23:23 -0400

    add config k:v to terraform

--- Terraform/modules/metadata_app_config/variables.tf
+++ Terraform/modules/metadata_app_config/variables.tf
@@ -2,3 +2,8 @@ variable "resources_common_name" {
   type        = string
   description = "Name shared between resources"
 }
+
+variable "return_properties" {
+  type        = list(string)
+  description = "List of strings containing properties of media files for app to return"
+}
